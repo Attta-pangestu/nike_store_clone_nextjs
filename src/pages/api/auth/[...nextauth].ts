60dba8842afa6b03d6084abe238a2036d213a186
atha <athabarokahfc@gmail.com>
2024-02-29 23:28:39 +0700

    fix:next secret key configuration

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,7 +8,7 @@ const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_SECRET,
   providers: [
     CredentialsProvider({
       type: "credentials",
